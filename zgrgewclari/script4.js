b85d7bd6788d97e2e0d36b3160d53af45315d209
ClariHealth <sohailouaby1@gmail.com>
2026-03-16 17:42:32 +0000

    Add files via upload

---
+++ zgrgewclari/script4.js
@@ -0,0 +1,9 @@
+document.querySelector("#section3").addEventListener("click", function(){
+    window.location = "http://127.0.0.1:3002/stomachache.html?serverWindowId=ba6aa646-6e0b-4f24-863d-5102994bfd45"
+})
+document.querySelector("#section4").addEventListener("click", function(){
+    window.location = "http://127.0.0.1:3002/constipation.html?serverWindowId=ec2d0eab-9fb5-45dd-aa9f-9fb23a8fc277"
+})
+document.querySelector(".backButton").addEventListener("click", function(){
+    window.location = "http://127.0.0.1:3002/ClariHealthBodyParts.html?serverWindowId=a06140f1-7607-423a-b285-3d63108d9fdf"
+})
