69756c881c7c5fb4ca3c88231d0c012f9c6e4297
ClariHealth <sohailouaby1@gmail.com>
2026-03-16 17:45:19 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// will select the button and wait until a click is detected
+document.querySelector(".button").addEventListener("click", function(){
+    window.location = "http://127.0.0.1:3000/ClariHealthBodyParts.html?serverWindowId=7698d03e-eca2-4c5b-9917-f1e96d8b0db6"
+})
