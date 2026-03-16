acb3e53bc49989dc5c4e49b2f6276ddb126337f9
ClariHealth <sohailouaby1@gmail.com>
2026-03-16 17:44:24 +0000

    Delete zgrgewclari directory

--- zgrgewclari/script2.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#section1").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3000/sinus.html?serverWindowId=bcff5f05-0494-4f6b-b096-a2204a9bf45a"
-})
-document.querySelector("#section2").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3000/headache.html?serverWindowId=f54154a3-9f0e-4220-a987-f98370122190"
-})
-document.querySelector(".backButton").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3002/ClariHealthBodyParts.html?serverWindowId=a06140f1-7607-423a-b285-3d63108d9fdf"
-})
--- zgrgewclari/script3.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#section5").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3002/muscelpain.html?serverWindowId=d21d06aa-352d-4c94-9bd3-f8557326d161"
-})
-document.querySelector("#section6").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3002/soarness.html?serverWindowId=d7e59751-2d3b-4c52-924c-16fdbc6bce19"
-})
-document.querySelector(".backButton").addEventListener("click", function(){
-    window.location = "http://127.0.0.1:3002/ClariHealthBodyParts.html?serverWindowId=a06140f1-7607-423a-b285-3d63108d9fdf"
-})
